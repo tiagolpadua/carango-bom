5e052d3a27a2cf1e022e2b2d5c9061656fd162ba
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2021-04-12 22:45:32 -0300

    menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { AppBar, Container, CssBaseline, Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Container, CssBaseline, Divider, Drawer, List, ListItem, ListItemText, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
-import CadastroUsuario from './pages/CadastroUsuario';
 import CadastroMarca from './pages/CadastroMarca';
+import CadastroUsuario from './pages/CadastroUsuario';
 import CadastroVeiculo from './pages/CadastroVeiculo';
+import Dashboard from './pages/Dashboard';
 import ListagemVeiculos from './pages/ListagemVeiculos';
 import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import Pagina404 from './pages/Pagina404';
 
 const drawerWidth = 240;
 
@@ -38,48 +40,78 @@ function App() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" noWrap>
-            Carango Bom
+    <Router>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" noWrap>
+              Carango Bom
           </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
-        <div className={classes.toolbar} />
-        <Divider />
-        <List>
-          {['Logar', 'Dashboard', 'Lista de Veículos', 'Cadastro de Veículo', 'Cadastro de Marca'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['Sair'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <Container component="article" maxWidth="sm">
-          <Login></Login>
-        </Container>
-      </main>
-    </div>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          anchor="left"
+        >
+          <div className={classes.toolbar} />
+          <Divider />
+          <List>
+            {[
+              ['/login', 'Entrar'],
+              ['/dashboard', 'Dashboard'],
+              ['/', 'Lista de Veículos'],
+              ['/cadastro-veiculo', 'Cadastro de Veículo'],
+              ['/cadastro-marca', 'Cadastro de Marca']
+            ].map((item, index) => (
+              <ListItem button component={Link} to={item[0]} key={item[1]}>
+                <ListItemText primary={item[1]} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {[['/logoff', 'Sair']].map((item, index) => (
+              <ListItem button component={Link} to={item[0]} key={item[1]}>
+                <ListItemText primary={item[1]} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Container component="article" maxWidth="sm">
+            <Switch>
+              <Route exact path="/">
+                <ListagemVeiculos></ListagemVeiculos>
+              </Route>
+              <Route path="/login">
+                <Login></Login>
+              </Route>
+              <Route path="/dashboard">
+                <Dashboard></Dashboard>
+              </Route>
+              <Route path="/cadastro-usuario">
+                <CadastroUsuario></CadastroUsuario>
+              </Route>
+              <Route path="/cadastro-veiculo">
+                <CadastroVeiculo></CadastroVeiculo>
+              </Route>
+              <Route path="/cadastro-marca">
+                <CadastroMarca></CadastroMarca>
+              </Route>
+              <Route>
+                <Pagina404></Pagina404>
+              </Route>
+            </Switch>
+          </Container>
+        </main>
+      </div>
+    </Router>
   );
 }
 
