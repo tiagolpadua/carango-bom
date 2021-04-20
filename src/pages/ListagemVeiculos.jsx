f73b7280237852011cbb0abd16221bd60e9546d5
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2021-04-19 23:39:54 -0300

    alteração e exclusão

--- src/pages/ListagemVeiculos.jsx
+++ src/pages/ListagemVeiculos.jsx
@@ -1,10 +1,11 @@
-import { Fab, makeStyles } from '@material-ui/core';
+import { Button, Fab, makeStyles } from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
-import React, { useContext, useEffect, useState } from 'react';
-import VeiculoService from '../services/VeiculoService';
 import AddIcon from '@material-ui/icons/Add';
-import UsuarioLogadoContext from '../contexts/UsuarioLogadoContext';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
+import Confirmacao from '../components/Confirmacao';
+import UsuarioLogadoContext from '../contexts/UsuarioLogadoContext';
+import VeiculoService from '../services/VeiculoService';
 
 const colunas = [
     { field: 'marca', headerName: 'Marca', width: 200 },
@@ -17,14 +18,40 @@ const useStyles = makeStyles((theme) => ({
         position: 'absolute',
         bottom: '100px',
         right: '100px',
+    },
+    actionsToolbar: {
+        float: 'right'
+    },
+    actions: {
+        top: '10px',
+        marginLeft: '10px',
     }
 }));
 
 function ListagemVeiculos() {
     const [veiculos, setVeiculos] = useState([]);
+    const [veiculoSelecionado, setVeiculoSelecionado] = useState();
     const classes = useStyles();
     const { usuarioLogado } = useContext(UsuarioLogadoContext);
     const history = useHistory();
+    const [openDialog, setOpenDialog] = React.useState(false);
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
+
+    function alterar() {
+
+    }
+
+    function excluir() {
+        setOpenDialog(true);
+    }
+
+    function handleConfirmDialog() {
+        setOpenDialog(false);
+        window.alert("Registro excluído!");
+    }
 
     useEffect(() => {
         VeiculoService.listar()
@@ -40,11 +67,46 @@ function ListagemVeiculos() {
 
     return (
         <div style={{ height: 300, width: '100%' }}>
-            <DataGrid rows={veiculos} columns={colunas} />
-            {usuarioLogado && <Fab color="primary" aria-label="add" className={classes.fab} onClick={() => history.push('/cadastro-veiculo')}>
-                <AddIcon />
-            </Fab>}
-        </div>
+            <DataGrid
+                onRowSelected={gridSelection => setVeiculoSelecionado(gridSelection.data)}
+
+                rows={veiculos} columns={colunas} />
+            {
+                usuarioLogado &&
+                <>
+                    <div className={classes.actionsToolbar}>
+                        <Button
+                            className={classes.actions}
+                            variant="contained"
+                            color="secondary"
+                            disabled={!veiculoSelecionado}
+                            onClick={() => excluir()}>
+                            Excluir
+                        </Button>
+                        <Button
+                            className={classes.actions}
+                            variant="contained"
+                            color="primary"
+                            disabled={!veiculoSelecionado}
+                            onClick={() => alterar()}>
+                            Alterar
+                        </Button>
+                    </div>
+
+                    <Fab color="primary" aria-label="add" className={classes.fab} onClick={() => history.push('/cadastro-veiculo')}>
+                        <AddIcon />
+                    </Fab>
+                </>
+            }
+
+            <Confirmacao
+                open={openDialog}
+                onClose={handleCloseDialog}
+                onConfirm={handleConfirmDialog}
+                title='Excluir Veículo'
+                content='Confirma a exclusão?'
+            />
+        </div >
     );
 }
 
